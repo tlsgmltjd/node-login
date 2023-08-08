203649edfd74ec731d1d58e9ca1fbe48c1f2329c
신희성 <vvwv3092@gmail.com>
2023-08-08 12:54:42 +0900

    Add login system

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
+
+const login = (e) => {
+  e.preventDefault();
+
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+
+  console.log(req);
+};
+
+loginBtn.addEventListener("click", login);
