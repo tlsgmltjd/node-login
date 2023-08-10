0c4fabfb63dad526ff69d86e23e3f800606f5363
신희성 <vvwv3092@gmail.com>
2023-08-11 04:50:02 +0900

    Add post API

--- app/app.js
+++ app/app.js
@@ -11,7 +11,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json()); // JSON 데이터 처리
+app.use(express.urlencoded({ extended: true })); // URL 인코딩된 데이터 처리
 
-app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+app.use("/", home);
 
 module.exports = app;
