65b62d43492a1b261ae3dc8e6756d3a3ed629b98
신희성 <vvwv3092@gmail.com>
2023-08-12 23:26:53 +0900

    Add User Model

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -13,26 +13,25 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const password = req.body.password;
-
-    const users = UserStorage.getUsers("id", "password");
-
-    const response = {};
-
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-
-      if (users.password[idx] === password) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.messege = "로그인에 실패하였습니다!";
+    const user = new User(req.body);
+    const response = user.login();
 
     return res.json(response);
+
+    //   const id = req.body.id;
+    //   const password = req.body.password;
+    //   const users = UserStorage.getUsers("id", "password");
+    //   const response = {};
+    //   if (users.id.includes(id)) {
+    //     const idx = users.id.indexOf(id);
+    //     if (users.password[idx] === password) {
+    //       response.success = true;
+    //       return res.json(response);
+    //     }
+    //   }
+    //   response.success = false;
+    //   response.messege = "로그인에 실패하였습니다!";
+    //   return res.json(response);
   },
 };
 
